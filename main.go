3730d22ec80e4063f84ecb743ab11c076bb6ce41
melissamillares <diana.millares@correounivalle.edu.co>
2019-12-30 20:00:22 -0500

    connection with database

--- main.go
+++ main.go
@@ -3,34 +3,62 @@ package main
 import (
 	"net"
 	"net/url"
-	"fmt"		
-	"github.com/likexian/whois-go"		
+	"net/http"
+
+	"html/template"
+
+	"fmt"
 	"strings"
+
+	"github.com/likexian/whois-go"		
+
+	"database/sql"
+	_ "github.com/lib/pq"
+	//"github.com/go-chi/chi"	
 )
 
-// variable representing the url without http
-var hoststring string
+var input string
+// 
+var tmpl = template.Must(template.ParseFiles("index.html"))
 
 func main() {
-	//fmt.Println(isURL("https://google.com"))		
-	fmt.Print("ingrese: ")
-	var input string
+	//fmt.Println(isURL("https://google.com"))	
+
+	/* r := chi.NewRouter()
+
+	r.Get("/", h)
+	http.ListenAndServe(":3000", r) */
+
+	database := connDB()
+
+	fmt.Print("ingrese: ")	
 	fmt.Scanf("%s", &input)
 
 	outputURL := isURL(input)
-	fmt.Println(outputURL)	
 
-	outputHost := hostName(input)
-	fmt.Println(outputHost)
+	if outputURL {
+		outputHost := hostName(input)
+		//fmt.Println(outputHost)
 
-	outputIPs := getIP(outputHost)
-	fmt.Println(outputIPs)	
-	
-	outputInfoName := getInfoWhoIs("OrgName: ", outputIPs)
-	fmt.Println(outputInfoName)
+		// array with the server IPs 
+		outputIPs := getIP(outputHost)			
+		// server owner
+		outputInfoName := getInfoWhoIs("OrgName: ", outputIPs)
+		fmt.Println(outputInfoName)
+		// server country
+		outputInfoCountry := getInfoWhoIs("Country: ", outputIPs)
+		fmt.Println(outputInfoCountry)
 
-	outputInfoCountry := getInfoWhoIs("Country: ", outputIPs)
-	fmt.Println(outputInfoCountry)
+		// insert data
+		insertDomainsDB(database, outputHost, "", "")
+		for _, value := range outputIPs {  		
+			insertServersDB(database, value, "A", outputInfoCountry, outputInfoName, outputHost)	
+		}
+		//query(database)
+	}	
+
+	// close the connection to the DB
+	defer database.Close()
 }
 
 // 
@@ -67,7 +95,7 @@ func getIP(urlString string) []net.IP {
 	}	
 }
 
-// gets the information s from the query whois
+// get the information s from the query whois
 func getInfoWhoIs(s string, ips []net.IP) string {
 	for _, value := range ips {
 		result, err := whois.Whois(value.String())
@@ -78,7 +106,6 @@ func getInfoWhoIs(s string, ips []net.IP) string {
 		}
 
 		splitResult := strings.Split(result, "\n")
-		//fmt.Printf("%q\n", splitResult)
 				
 		// buscar en splitresult orgname
 		for _, val := range splitResult {
@@ -91,4 +118,78 @@ func getInfoWhoIs(s string, ips []net.IP) string {
 		}		
 	}
 	return ""	
+}
+
+func getTittle() string {
+	
+	return ""
+}
+
+func connDB() *sql.DB {      
+	db, err := sql.Open("postgres",
+		"postgresql://maxroach@localhost:26257/prueba_truora?ssl=true&sslmode=require&sslrootcert=certs/ca.crt&sslkey=certs/client.maxroach.key&sslcert=certs/client.maxroach.crt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	// close the connection to the DB
+	//defer db.Close()
+	
+	// Create the "servers" table.
+	_, ers := db.Exec("CREATE TABLE IF NOT EXISTS servers(address STRING PRIMARY KEY, ssl_grade STRING, country STRING, owner STRING, domain STRING, FOREIGN KEY (domain) REFERENCES domains(uri))")
+	// Create the "domains" table
+	_, erd := db.Exec("CREATE TABLE IF NOT EXISTS domains(url STRING PRIMARY KEY, logo STRING, tittle STRING)")
+
+	if ers != nil || erd != nil {
+		fmt.Println(ers)
+		fmt.Println(erd)
+	}
+	
+	return db
+} 
+
+func insertDomainsDB(db *sql.DB, url string, logo string, tittle string) {
+	_, err := db.Exec("INSERT INTO domains (url, logo, tittle) VALUES ($1, $2, $3);", url, logo, tittle) 	
+
+	if err != nil {
+		fmt.Println("Error inserting data")
+	}
+}
+
+// insert the data to the servers database, db: the database
+func insertServersDB(db *sql.DB, address net.IP, ssl_grade string, country string, owner string, domain string) {		
+	a := address.String()
+	_, err := db.Exec("INSERT INTO servers (address, ssl_grade, country, owner, domain) VALUES ($1, $2, $3, $4, $5);", a, ssl_grade, country, owner, domain)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func query(db *sql.DB) {	
+	rows, err := db.Query("SELECT address, ssl_grade, country, owner FROM servers")
+	
+    if err != nil {
+        fmt.Println(err)
+	}
+	
+	defer rows.Close()
+	
+	fmt.Println(rows)
+}
+
+
+func h(w http.ResponseWriter, r *http.Request) {
+	tmpl.Execute(w, nil)		
+	
+	if r.Method == "POST" {
+		if err := r.ParseForm(); err != nil {
+            fmt.Fprintf(w, "ParseForm() err: %v", err)
+            return
+        }
+
+		pageurl := r.FormValue("purl")
+		
+		fmt.Fprintf(w, pageurl)		
+	}  
 }
