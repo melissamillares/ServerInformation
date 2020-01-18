c8cb16a3c88b63c774737f936b7e7ae7d7995d1d
melissamillares <diana.millares@correounivalle.edu.co>
2020-01-18 12:31:47 -0500

    fix displaying search information

--- front/src/views/Search.vue
+++ front/src/views/Search.vue
@@ -8,7 +8,7 @@
     <b-container fluid>         
         <b-card  
             overlay
-            img-src="https://cdn.pixabay.com/photo/2016/03/26/13/09/notebook-1280538_960_720.jpg" 
+            img-src="https://picsum.photos/900/132/?image=3" 
             text-variant="white"                               
             style="max-width: 100rem;"            
         >    
@@ -38,14 +38,17 @@
                 </b-container>
 
                 <b-container fluid>
-                    <div v-if="urlEmpty" class="w-25 bg-info text-light">                    
+                    <div v-if="urlEmpty" class="w-25 bg-info text-dark">                    
                         <p class="mt-1"> ⬆️ Attention! This field cannot be empty</p>
                     </div>
-                    <div v-if="error" class="w-25 bg-info text-light">
+                    <div v-if="error" class="w-25 bg-info text-dark">
                         <p class="mt-1"> ⬆️ Attention! There is an error in the URL</p>                    
                     </div>
                 </b-container>
+            </b-card-text>
 
+            <b-card-text>
+                <div><br></div>
                 <b-container fluid>
                     <div class="mt-3">
                         <b-button block href="#" v-b-toggle.accordion-1 variant="dark">Domain Info</b-button>
@@ -55,31 +58,31 @@
                                 <b-tbody>  
                                     <b-tr>
                                         <b-th class="text-right" variant="dark" colspan="1">URL</b-th>
-                                        <b-td class="text-left text-light" text-light colspan="5">{{ url }}</b-td>
+                                        <b-td class="text-left text-dark" text-light colspan="5">{{ url }}</b-td>
                                     </b-tr>                                    
                                     <b-tr>
                                         <b-th class="text-right" variant="dark" colspan="1">Servers Changed</b-th> 
-                                        <b-td class="text-left text-light" colspan="5">{{ changed }}</b-td>
+                                        <b-td class="text-left text-dark" colspan="5">{{ changed }}</b-td>
                                     </b-tr>
                                     <b-tr>
                                         <b-th class="text-right" variant="dark" colspan="1">SSL Grade</b-th> 
-                                        <b-td class="text-left text-light" colspan="5">{{ ssl_grade }}</b-td>
+                                        <b-td class="text-left text-dark" colspan="5">{{ ssl_grade }}</b-td>
                                     </b-tr>
                                     <b-tr>
                                         <b-th class="text-right" variant="dark" colspan="1">Previous SSL</b-th> 
-                                        <b-td class="text-left text-light" colspan="5">{{ previous }}</b-td>
+                                        <b-td class="text-left text-dark" colspan="5">{{ previous }}</b-td>
                                     </b-tr>
                                     <b-tr> 
                                         <b-th class="text-right" variant="dark" colspan="1">Logo</b-th> 
-                                        <b-td class="text-left text-light" colspan="5">{{ logo }}</b-td>
+                                        <b-td class="text-left text-dark" colspan="5">{{ logo }}</b-td>
                                     </b-tr>                           
                                     <b-tr> 
                                         <b-th class="text-right" variant="dark" colspan="1">Title</b-th> 
-                                        <b-td class="text-left text-light" colspan="5">{{ title }}</b-td>
+                                        <b-td class="text-left text-dark" colspan="5">{{ title }}</b-td>
                                     </b-tr> 
                                     <b-tr>
                                         <b-th class="text-right" variant="dark" colspan="1">Is Down</b-th>     
-                                        <b-td class="text-left text-light" colspan="5">{{ down }}</b-td>
+                                        <b-td class="text-left text-dark" colspan="5">{{ down }}</b-td>
                                     </b-tr>                                                                                                                                                                                                  
                                 </b-tbody>
                             </b-table-simple> 
@@ -89,20 +92,15 @@
                     <div>
                         <!-- Table for the servers -->
                         <b-button block href="#" v-b-toggle.accordion-2 variant="dark">Servers</b-button>
-                        <b-collapse id="accordion-2" visible accordion="my-accordion" role="tabpanel">
-                            <b-table class="text-light" variant="dark" hover :items="servers"></b-table>
+                        <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+                            <b-table class="text-dark" variant="dark" hover :items="servers"></b-table>
                         </b-collapse> 
                     </div>                    
                 </b-container>
-
-            </b-card-text>            
+            </b-card-text>
+            <div><br></div>            
         </b-card>        
-    </b-container> 
-    <div >
-        <p >
-
-        </p>
-    </div>            
+    </b-container>                
   </div>      
 </template>
     
