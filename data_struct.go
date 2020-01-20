c05324a95b1fed7619b135328b83a206d5f439d5
melissamillares <diana.millares@correounivalle.edu.co>
2020-01-19 21:01:57 -0500

    delete comments

--- data_struct.go
+++ data_struct.go
@@ -7,40 +7,37 @@ import (
 // struct with the server information, the info has to be parsed to JSON
 // DomainID is the id of the domain the server belongs to 
 type Server struct { 
-	ID int 				`json:"-"`   
-	Address string    	`json:"address"`
-	SSL_grade string  	`json:"ssl_grade"`
-	Country string   	`json:"country"`
-	Owner string      	`json:"owner"` 
-	DomainID int 		`json:"-"` 
-	Domain string 	  	`json:"-"`
-	Created time.Time 	`json:"-"`
-	Updated time.Time 	`json:"-"`
+	ID int `json:"-"`   
+	Address string `json:"address"`
+	SSL_grade string `json:"ssl_grade"`
+	Country string `json:"country"`
+	Owner string `json:"owner"` 
+	DomainID int `json:"-"` 
+	Domain string `json:"-"`
+	Created time.Time `json:"-"`
+	Updated time.Time `json:"-"`
 }
 
-// struct with the server information
+// struct with the domain information
 type Domain struct {
-	ID int 					`json:"-"` 
-	URL string				`json:"url"`
-	Servers []Server		`json:"servers"`
-	Servers_Changed bool    `json:"servers_changed"`
-	SSL string				`json:"ssl_grade"`
-	Previous_SSL string 	`json:"previous_ssl_grade"`
-	Logo string 			`json:"logo"`
-	Title string 			`json:"title"`
-	Is_Down bool 			`json:"is_down"`	
-	Created time.Time		`json:"-"`
-	Updated time.Time 		`json:"-"`
+	ID int `json:"-"` 
+	URL string `json:"url"`
+	Servers []Server `json:"servers"`
+	Servers_Changed bool `json:"servers_changed"`
+	SSL string `json:"ssl_grade"`
+	Previous_SSL string `json:"previous_ssl_grade"`
+	Logo string `json:"logo"`
+	Title string `json:"title"`
+	Is_Down bool `json:"is_down"`	
+	Created time.Time `json:"-"`
+	Updated time.Time `json:"-"`
 }
 
-// struct with a Domain array
 type Item struct {	
-	URL string			`json:"url"`	
-	Domain Domain 		`json:"info"`
-	// sacar los datos por separado en lugar de poner todo el domain	
+	URL string `json:"url"`	
+	Domain Domain `json:"info"`		
 }
 
 type Items struct {		
-	Items []Item 		`json:"items"`
-	// sacar los datos por separado en lugar de poner todo el domain	
+	Items []Item `json:"items"`		
 }
